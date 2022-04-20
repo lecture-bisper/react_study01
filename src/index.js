907fb8be93d1807a84c78575f42bfa1b410d9ef3
lecture-bisper <bisper@outlook.kr>
2022-04-20 16:48:51 +0900

    리액트 기본파일인 index.html, index.js 파일에 대한 주석 추가.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import App2 from './App2';
+// import Potato from './Potato';
+
+// ReactDOM : 리액트의 가상돔을 의미
+// createRoot : 리액트의 가상돔을 생성
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// render : 리액트의 가상돔을 사용하여 실제 화면을 그려줌
+// StrictMode : 기본 자바스크립트의 언어 규칙을 강력한 규칙을 적용함
+// <App /> : 사용자 컴포넌트, 실제 리액트를 이용하여 생성한 화면
+
+// 리액트의 기본 구성요소 3가지
+// 컴포넌트 : 실제 구현될 화면, js 파일
+// 리액트는 컴포넌트를 여러개 생성하여 컴포넌트를 조합시켜 하나의 화면을 구성함
+// jsp나 spring에서 header, footer 를 미리 만들어놓고 필요한 부분에서 로딩하여 사용하는 것과 동일한 형태
+// JSX : html과 js가 합해진 형태의 문법
+// 화면을 그리기위한 소스코드를 전부 순수 자바스크립트로 생성하기에는 힘들기 때문에 JS + XML 로 이루어진 JSX라는 문법을 사용하여 html 화면을 쉽게 구성함
+// PROPS : 각 컴포넌트끼리 데이터를 주고 받기 위한 객체, 일종의 매개변수
 root.render(
   <React.StrictMode>
+    <App2 />
+    {/* <Potato /> */}
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
